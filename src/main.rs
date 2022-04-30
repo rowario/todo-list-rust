5cfaa4f66a6b383e5a3b4829dff7e2176317bbc6
Rowario <r0wario@ya.ru>
2022-04-30 14:52:24 +0300

    moved already working stuff to TUI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod database;
 
 use database::*;
-use std::{io};
-use std::io::Result;
+use std::{io::{
+    self,
+    Result
+}};
 use crossterm::{event, execute};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use tui::{
@@ -21,7 +23,9 @@ use crossterm::{
 use crossterm::event::KeyCode;
 use rusqlite::Connection;
 use tui::backend::CrosstermBackend;
-use tui::widgets::{List, ListItem};
+use tui::layout::Rect;
+use tui::widgets::{Clear, List, ListItem, Paragraph};
+use unicode_width::UnicodeWidthStr;
 
 enum Screen {
     New,
@@ -34,6 +38,7 @@ struct App {
     todos: Vec<Todo>,
     current_index: usize,
     current_screen: Screen,
+    input_value: String,
 }
 
 impl App {
@@ -43,12 +48,17 @@ impl App {
             todos: vec![],
             current_index: 0,
             current_screen: Screen::Todos,
+            input_value: String::new(),
         }
     }
 
     fn load_todos(&mut self) {
         let todos = get_todos(&self.db).expect("Cannot get todos");
         self.todos = todos;
+    }
+
+    fn set_screen(&mut self, screen: Screen) {
+        self.current_screen = screen;
     }
 
     fn next(&mut self) {
@@ -63,7 +73,7 @@ impl App {
         }
     }
 
-    fn toggle_todo(&mut self) {
+    fn toggle(&mut self) {
         if let Some(todo) = self.todos.get_mut(self.current_index) {
             if toggle_todo(&self.db, todo.id).is_ok() {
                 todo.toggle();
@@ -71,32 +81,61 @@ impl App {
         }
     }
 
+    fn create(&mut self) {
+        if let Ok(todo) = new_todo(&self.db, self.input_value.as_str()) {
+            self.input_value.clear();
+            self.todos.push(todo);
+        }
+    }
+
+    fn delete(&mut self) {
+        if let Some(todo) = self.todos.get(self.current_index) {
+            if delete_todo(&self.db, todo.id).is_ok() {
+                self.todos.remove(self.current_index);
+            }
+        }
+    }
+
     fn draw_ui<B: Backend>(&self, f: &mut Frame<B>) {
         match self.current_screen {
-            Screen::Todos => {
-                let chunks = Layout::default()
-                    .direction(Direction::Horizontal)
-                    .margin(1)
-                    .constraints(
-                        [
-                            Constraint::Percentage(30),
-                            Constraint::Percentage(70)
-                        ].as_ref()
-                    )
-                    .split(f.size());
-                let block = Block::default()
-                    .title("TODOs")
-                    .borders(Borders::ALL);
-                let list = List::new(self.get_todos_list()).block(block);
-                f.render_widget(list, chunks[0]);
-                let block = Block::default()
-                    .title("Notes")
-                    .borders(Borders::ALL);
-                f.render_widget(block, chunks[1]);
-            }
+            Screen::Todos => self.draw_todos(f),
             Screen::Stats => {}
-            Screen::New => {}
+            Screen::New => {
+                self.draw_todos(f);
+                self.draw_new(f);
+            }
         }
+    }
+
+    fn draw_todos<B: Backend>(&self, f: &mut Frame<B>) {
+        let chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(30),
+                    Constraint::Percentage(70)
+                ].as_ref()
+            )
+            .split(f.size());
+        let block = Block::default()
+            .title("TODOs")
+            .borders(Borders::ALL);
+        let list = List::new(self.get_todos_list()).block(block);
+        f.render_widget(list, chunks[0]);
+        let block = Block::default()
+            .title("Notes")
+            .borders(Borders::ALL);
+        f.render_widget(block, chunks[1]);
+    }
+
+    fn draw_new<B: Backend>(&self, f: &mut Frame<B>) {
+        let block = Paragraph::new(self.input_value.as_ref())
+            .block(Block::default().title("New TODO").borders(Borders::ALL));
+        let area = input_rect(60, f.size());
+        f.render_widget(Clear, area);
+        f.render_widget(block, area);
+        f.set_cursor(area.x + self.input_value.width() as u16 + 1, area.y + 1);
     }
 
     fn get_todos_list(&self) -> Vec<ListItem> {
@@ -127,12 +166,36 @@ fn main() -> Result<()> {
         terminal.draw(|f| app.draw_ui(f))?;
 
         if let Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Char('q') => break,
-                KeyCode::Char('j') => app.next(),
-                KeyCode::Char('k') => app.previous(),
-                KeyCode::Char('x') => app.toggle_todo(),
-                _ => {}
+            match app.current_screen {
+                Screen::New => {
+                    match key.code {
+                        KeyCode::Char(c) => app.input_value.push(c),
+                        KeyCode::Esc => {
+                            app.input_value.clear();
+                            app.set_screen(Screen::Todos);
+                        }
+                        KeyCode::Backspace => {
+                            app.input_value.pop();
+                        },
+                        KeyCode::Enter => {
+                            app.set_screen(Screen::Todos);
+                            app.create();
+                        },
+                        _ => {}
+                    }
+                }
+                Screen::Todos => {
+                    match key.code {
+                        KeyCode::Char('q') => break,
+                        KeyCode::Char('k') => app.previous(),
+                        KeyCode::Char('j') => app.next(),
+                        KeyCode::Char('x') => app.toggle(),
+                        KeyCode::Char('d') => app.delete(),
+                        KeyCode::Char('n') => app.set_screen(Screen::New),
+                        _ => {}
+                    }
+                }
+                Screen::Stats => {}
             }
         }
     }
@@ -146,4 +209,30 @@ fn main() -> Result<()> {
     terminal.show_cursor()?;
 
     Ok(())
+}
+
+fn input_rect(percent_x: u16, r: Rect) -> Rect {
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Percentage((100 - Constraint::Length(3).apply(3)) / 2),
+                Constraint::Length(3),
+                Constraint::Percentage((100 - Constraint::Length(3).apply(3)) / 2),
+            ]
+                .as_ref(),
+        )
+        .split(r);
+
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(
+            [
+                Constraint::Percentage((100 - percent_x) / 2),
+                Constraint::Percentage(percent_x),
+                Constraint::Percentage((100 - percent_x) / 2),
+            ]
+                .as_ref(),
+        )
+        .split(popup_layout[1])[1]
 }
