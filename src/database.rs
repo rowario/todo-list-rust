8b94416c4055972cc9211c5b7f24c2715d417b94
Rowario <r0wario@ya.ru>
2022-05-07 00:00:20 +0300

    rewrite database,todo,day,shortday implementations

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,5 @@
 use rusqlite::{Connection, Result};
 
-#[derive(Debug)]
 pub struct Todo {
     pub id: i64,
     pub position: i64,
@@ -10,6 +9,41 @@ pub struct Todo {
 }
 
 impl Todo {
+    pub fn new(db: &Connection, text: &str, day_id: i64) -> Result<Self> {
+        db.execute(
+            "INSERT INTO todos (text, completed, day_id) VALUES (?1, 0, ?2)",
+            &[text, day_id.to_string().as_str()],
+        )?;
+        let last_id = db.last_insert_rowid();
+        db.execute(
+            "UPDATE todos SET position = ?1 WHERE id = ?2",
+            &[&last_id, &last_id],
+        )?;
+        Ok(Self {
+            id: db.last_insert_rowid(),
+            day_id,
+            position: last_id,
+            text: text.to_string(),
+            completed: false,
+        })
+    }
+
+    pub fn get_all(db: &Connection, day_id: i64) -> Result<Vec<Self>> {
+        let mut stmt = db.prepare("SELECT id, day_id, position, text, completed FROM todos WHERE day_id = ?1 ORDER BY position ASC")?;
+        let todos: Vec<Self> = stmt.query_map([day_id], |row| {
+            Ok(Self {
+                id: row.get(0)?,
+                day_id: row.get(1)?,
+                position: row.get(2)?,
+                text: row.get(3)?,
+                completed: row.get(4)?,
+            })
+        })?
+            .filter_map(Result::ok)
+            .collect();
+        Ok(todos)
+    }
+
     pub fn get_text(&self) -> String {
         if self.completed {
             format!("{} {}", "[x]", self.text)
@@ -18,12 +52,31 @@ impl Todo {
         }
     }
 
-    pub fn toggle(&mut self) {
+    pub fn toggle(&mut self, db: &Connection) -> Result<()> {
+        db.execute(
+            "UPDATE todos SET completed = 1 - completed WHERE id = ?1",
+            &[&self.id],
+        )?;
         self.completed = !self.completed;
+        Ok(())
+    }
+
+    pub fn delete_todo(&self, db: &Connection) -> Result<()> {
+        db.execute("DELETE FROM todos WHERE id = ?1", &[&self.id])?;
+        Ok(())
+    }
+
+    pub fn update_positions(db: &Connection, todos: &[Self]) -> Result<()> {
+        for (i, todo) in todos.iter().enumerate() {
+            db.execute(
+                "UPDATE todos SET position = ?1 WHERE id = ?2",
+                &[&i.to_string().as_str(), &todo.id.to_string().as_str()],
+            )?;
+        }
+        Ok(())
     }
 }
 
-#[derive(Debug)]
 pub struct Day {
     pub id: i64,
     pub count_todos: i64,
@@ -33,12 +86,94 @@ pub struct Day {
     pub todos: Vec<Todo>,
 }
 
+impl Day {
+    pub fn new(db: &Connection, date: &str) -> Result<Self> {
+        db.execute(
+            "INSERT INTO days (count_todos, done_todos, notes, date) VALUES (0,0,'',?1)",
+            &[date],
+        )?;
+        let id = db.last_insert_rowid();
+        Ok(Self {
+            id,
+            count_todos: 0,
+            done_todos: 0,
+            notes: String::new(),
+            date: String::from(date),
+            todos: vec![],
+        })
+    }
+
+    pub fn get(db: &Connection, day_id: i64) -> Result<Self> {
+        let mut stmt = db.prepare("SELECT id, count_todos, done_todos, notes, date FROM days WHERE id = ?1 LIMIT 1")?;
+        let day = stmt.query_row([day_id], |r| {
+            let id = r.get(0)?;
+            let todos = Todo::get_all(db, id)?;
+            Ok(Day {
+                id,
+                count_todos: r.get(1)?,
+                done_todos: r.get(2)?,
+                notes: r.get(3)?,
+                date: r.get(4)?,
+                todos,
+            })
+        })?;
+        Ok(day)
+    }
+
+
+    pub fn set_notes(&mut self, db: &Connection, notes: &str) -> Result<()> {
+        db.execute(
+            "UPDATE days SET notes = ?1 WHERE id = ?2",
+            &[&notes, self.id.to_string().as_str()],
+        )?;
+        self.notes = String::from(notes);
+        Ok(())
+    }
+
+    pub fn update_counts(&mut self, db: &Connection) -> Result<()> {
+        self.count_todos = self.todos.len() as i64;
+        self.done_todos = self.todos.iter().filter(|t| t.completed).count() as i64;
+        db.execute(
+            "UPDATE days SET count_todos = ?1, done_todos = ?2 WHERE id = ?3",
+            &[&self.count_todos, &self.done_todos, &self.id],
+        )?;
+        Ok(())
+    }
+
+    pub fn add_todo(&mut self, db: &Connection, item: Todo) -> Result<()> {
+        self.todos.push(item);
+        self.update_counts(db)?;
+        Ok(())
+    }
+
+    pub fn remove_todo(&mut self, db: &Connection, index: usize) -> Result<()> {
+        self.todos.remove(index);
+        self.update_counts(db)?;
+        Ok(())
+    }
+}
+
 pub struct DayShort {
     pub id: i64,
     pub date: String,
 }
 
-pub fn init(path: &str) -> Result<Connection> {
+impl DayShort {
+    pub fn get_all(db: &Connection) -> Result<Vec<Self>> {
+        let mut stmt = db.prepare("SELECT id, date FROM days")?;
+        let days: Vec<Self> = stmt.query_map([], |r| {
+            Ok(DayShort {
+                id: r.get(0)?,
+                date: r.get(1)?,
+            })
+        })?
+            .filter_map(Result::ok)
+            .collect();
+        Ok(days)
+    }
+}
+
+pub fn init_connection(path: &str) -> Result<Connection> {
     let conn = Connection::open(path)?;
     conn.execute(
         "CREATE TABLE IF NOT EXISTS todos (
@@ -62,108 +197,3 @@ pub fn init(path: &str) -> Result<Connection> {
     )?;
     Ok(conn)
 }
-
-pub fn get_days(conn: &Connection) -> Result<Vec<DayShort>> {
-    let mut stmt = conn.prepare("SELECT id, date FROM days")?;
-    let days: Vec<DayShort> = stmt.query_map([], |r| {
-        Ok(DayShort {
-            id: r.get(0)?,
-            date: r.get(1)?,
-        })
-    })?
-        .filter_map(Result::ok)
-        .collect();
-    Ok(days)
-}
-
-pub fn get_day(conn: &Connection, day_id: i64) -> Result<Day> {
-    let mut stmt = conn.prepare("SELECT id, count_todos, done_todos, notes, date FROM days WHERE id = ?1 LIMIT 1")?;
-    let day = stmt.query_row([day_id], |r| {
-        let id = r.get(0)?;
-        let todos = get_todos(conn, id)?;
-        Ok(Day {
-            id,
-            count_todos: r.get(1)?,
-            done_todos: r.get(2)?,
-            notes: r.get(3)?,
-            date: r.get(4)?,
-            todos,
-        })
-    })?;
-    Ok(day)
-}
-
-pub fn new_day(conn: &Connection, date: &str) -> Result<Day> {
-    conn.execute(
-        "INSERT INTO days (count_todos, done_todos, notes, date) VALUES (0,0,'',?1)",
-        &[date],
-    )?;
-    let id = conn.last_insert_rowid();
-    Ok(Day {
-        id,
-        count_todos: 0,
-        done_todos: 0,
-        notes: String::new(),
-        date: String::from(date),
-        todos: vec![],
-    })
-}
-
-fn get_todos(conn: &Connection, day_id: i64) -> Result<Vec<Todo>> {
-    let mut stmt = conn.prepare("SELECT id, day_id, position, text, completed FROM todos WHERE day_id = ?1 ORDER BY position ASC")?;
-    let todos: Vec<Todo> = stmt.query_map([day_id], |row| {
-        Ok(Todo {
-            id: row.get(0)?,
-            day_id: row.get(1)?,
-            position: row.get(2)?,
-            text: row.get(3)?,
-            completed: row.get(4)?,
-        })
-    })?
-        .filter_map(Result::ok)
-        .collect();
-    Ok(todos)
-}
-
-pub fn new_todo(conn: &Connection, text: &str, day_id: i64) -> Result<Todo> {
-    conn.execute(
-        "INSERT INTO todos (text, completed, day_id) VALUES (?1, 0, ?2)",
-        &[text, day_id.to_string().as_str()],
-    )?;
-    let last_id = conn.last_insert_rowid();
-    conn.execute(
-        "UPDATE todos SET position = ?1 WHERE id = ?2",
-        &[&last_id, &last_id],
-    )?;
-    Ok(Todo {
-        id: conn.last_insert_rowid(),
-        day_id,
-        position: last_id,
-        text: text.to_string(),
-        completed: false,
-    })
-}
-
-pub fn toggle_todo(conn: &Connection, id: i64) -> Result<()> {
-    conn.execute(
-        "UPDATE todos SET completed = 1 - completed WHERE id = ?1",
-        &[&id],
-    )?;
-    Ok(())
-}
-
-pub fn delete_todo(conn: &Connection, id: i64) -> Result<()> {
-    conn.execute("DELETE FROM todos WHERE id = ?1", &[&id])?;
-    Ok(())
-}
-
-pub fn update_todos_positions(conn: &Connection, todos: &[Todo]) -> Result<()> {
-    for (i, todo) in todos.iter().enumerate() {
-        conn.execute(
-            "UPDATE todos SET position = ?1 WHERE id = ?2",
-            &[&i.to_string().as_str(), &todo.id.to_string().as_str()],
-        )?;
-    }
-    Ok(())
-}
-
